b4e8680b5cdf4893a817216480e83867d0f96356
satish2596 <satish.chandu@cmsedge.com>
2024-10-19 21:55:21 +0530

    Modified the test case in App.test.tsx

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = () => {
-    console.log("User Details are:", "Username: ",username, "\n","Password: ",password);
+    console.log(
+      "User Details are:",
+      "Username: ",
+      username,
+      "\n",
+      "Password: ",
+      password,
+    );
   };
 
   return (
     <div className="App">
-      <input 
-      type="text"
-      name="username"
-      placeholder='Enter the username'
-      value={username}
-      onChange={(e) => setUsername(e.target.value)}
+      <input
+        type="text"
+        name="username"
+        placeholder="Enter the username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
-      <input 
-      type="password"
-      name="password"
-      placeholder="Enter the password"
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
+      <input
+        type="password"
+        name="password"
+        placeholder="Enter the password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={handleSubmit} disabled={!username && !password}>Submit</button>
+      <button onClick={handleSubmit} disabled={!username && !password}>
+        Submit
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders input elements correctly', () => {
+test("renders input elements correctly", () => {
   render(<App />);
-  const txtElement = screen.getByRole('textbox');
+  const txtElement = screen.getByRole("textbox");
   expect(txtElement).toBeInTheDocument();
 });
 
-test('renders button componet correctly', () => {
+test("renders button componet correctly", () => {
   render(<App />);
-  const buttonEle = screen.getByRole('button');
-  expect(buttonEle).toBeInTheDocument();
+  const buttonEle = screen.getByRole("button");
+  expect(buttonEle).toHaveTextContent('Submit');
 });
